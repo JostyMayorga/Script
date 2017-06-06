df5e20606aa3cdd960d5676bd2961751317894c0
Josty <josty-12@hotmail.com>
2017-06-06 12:27:00 -0500

    Subir todo

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var sentences = document.getElementById('sentences');
+var buscar = document.getElementById('buscar');
+
+var palabra = "Lorem";
+
+var encontrado = false;
+
+function myFunction(obj) {
+  document.getElementById('Cont').style.display = 'none';
+  document.getElementById('B').innerHTML = "mostrar"
+
+  alert('A')
+
+  var image = document.createElement("img");
+  image.src = "img/visto.png";
+  var nuevaimg = document.createElement("p");
+  nuevaimg.appendChild(image);
+  document.getElementById('Cont').appendChild(nuevaimg);
+  
+  document.getElementById("myBtn").addEventListener("click", function(){
+    hide(obj)
+  });
+
+}
+
+
+
+function hide(obj) {
+
+  if (document.getElementById('B').innerHTML == "esconder") {
+    document.getElementById('B').innerHTML = "mostrar"
+    document.getElementById('Cont').style.display = 'none';
+
+
+
+
+  } else {
+    document.getElementById('B').innerHTML = "esconder"
+    document.getElementById('Cont').style.display = 'block';
+    document.getElementById('A').style.background = '#F55';
+    for (let i = 1; i <= document.getElementsByTagName('p').length; i++){
+    document.getElementsByTagName('p')[i].style.background = '#F05';
+    }
+  }
+
+
+}
+
+function printDOM(node, prefix) {
+  console.log(prefix + node.nodeName);
+  for (let i = 0; i < node.childNodes.length; i++) {
+    printDOM(node.childNodes[i], prefix + ' ')
+
+  }
+}
+printDOM(document, ' ')
